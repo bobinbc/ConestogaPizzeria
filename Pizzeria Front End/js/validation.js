6ec380dd67c6f8135bbb1351f4d18e8ba82d9807
Bobin Cherian <bobinbc@gmail.com>
2015-10-09 21:48:06 -0400

    Filling in Base

---
+++ Pizzeria Front End/js/validation.js
@@ -0,0 +1,16 @@
+
+//function isEmpty(str) {
+//    return (!str || 0 === str.length);
+//}
+//
+//function areEquivalent(value1, value2) {
+//    return (value1 === value2);
+//}
+//
+//function isChecked(inputSet) {
+//    
+//}
+//
+//function isEmailValid(email) {
+//    
+//}
